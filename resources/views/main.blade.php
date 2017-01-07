7e98b51e7c42caee79c2f31ecddada7162e04871
JulioDRF <juliod.rangelfarell@gmail.com>
2017-01-07 13:18:24 -0500

    Initial commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Grey Drake</title>
+
+         {{ Html::style('css/app.css') }}
+    </head>
+    <body>
+        <div class="nav bg-white-70">
+            <p class="title tc">
+                Grey Drake
+            </p>
+        </div>
+        <div class="flex-center position-ref full-height">
+            <div class="absolute top-2">
+                <div class="links relative top-2">
+                    <a href="mailto:juliod.rangelfarell@gmail.com">Email</a>
+                    <a href="https://github.com/JulioDRF/GreyDrake">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
